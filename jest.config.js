4b6596bf33de65b3f63c3197bacd5fb711645f48
John Drefahl <john@velobpa.com>
2026-02-24 03:41:12 -0600

    chore: update jest.config.js to v1.0.0

--- jest.config.js
+++ jest.config.js
@@ -1,45 +1,24 @@
-/**
- * Copyright (c) Velocity BPA, LLC
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
  * Licensed under the Business Source License 1.1
- * Commercial use requires a separate commercial license.
- * See LICENSE file for details.
  */
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/test/**/*.test.ts'],
   collectCoverageFrom: [
     'nodes/**/*.ts',
     'credentials/**/*.ts',
     '!**/*.d.ts',
-    '!**/node_modules/**',
+    '!**/index.ts',
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  roots: ['<rootDir>'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: {
-        module: 'commonjs',
-        target: 'ES2021',
-        lib: ['ES2021'],
-        esModuleInterop: true,
-        skipLibCheck: true,
-        strict: true,
-      },
-    }],
+    '^.+\.ts$': 'ts-jest',
   },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
+  testTimeout: 30000,
 };
